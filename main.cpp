041df089ec855c1241737bd3a7eb8920a05f49e6
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-09-26 17:05:53 -0700

    Fills Grocery List

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
+//Author: Jerm Deeezy (Rhodes)
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        bool q;
+        q = true;
+        string newItem;
+        do
+        {
+                
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if(input=='Q' || input=='q')
+                {
+                        q = false;
+                }
 
-  return 0;
+                if(input=='A' || input=='a')
+                {
+
+                        cout<<"What is the item? \n";
+                                if(numItems<5)
+                                {
+                                        cin>>newItem;
+                                        list[numItems] = newItem;
+                                        numItems++;
+                                }
+                                else if(numItems>=5)
+                                {
+                                        cout<<"You'll need a bigger list!\n";
+                                }
+
+                }
+           } while(q == true);
+
+
+
+        return 0;
 }
+
+
